ec598683e1d737025411c4544cc91ea235eb310c
DOhnezeit <dennis.ohnezeit@gmail.com>
2025-05-24 23:03:36 +0200

    Add folder path input and error handling in DupeFind
    - Implemented `getPath()` function to prompt user for a folder path.
    - Updated main function to check if the path exists and is a directory.
    - Added error handling for filesystem and general exceptions.
    - Updated project file to use C++20 standard.

--- DupeFind/DupeFind.cpp
+++ DupeFind/DupeFind.cpp
@@ -2,9 +2,57 @@
 
 #include <iostream>
 #include <filesystem>
+#include <string>
+
+std::string getPath();
+
 
 int main()
 {
-    std::cout << "DupeFind Duplicate File Finder Starting..." << std::endl;
+    std::cout << "DupeFind is ready!" << std::endl;
+    std::cout << "Please enter a folder path to scan: " << std::endl;
+
+	std::string folderPath = getPath();
+
+	// Check for correct path format
+
+	try
+	{
+		if (!std::filesystem::exists(folderPath))
+		{
+			std::cout << "Error: Path does not exist." << std::endl;
+			return 1;
+		}
+
+		if (!std::filesystem::is_directory(folderPath))
+		{
+			std::cout << "Error: Path is not a directory." << std::endl;
+		}
+
+		std::cout << "Scanning directory: " << folderPath << std::endl;
+
+		// TODO: Scan here
+
+	}
+
+	catch (const std::filesystem::filesystem_error& exception)
+	{
+		std::cout << "Filesystem error: " << exception.what() << std::endl;
+		return 1;
+	}
+	catch (const std::exception& exception)
+	{
+		std::cout << "Error: " << exception.what() << std::endl;
+		return 1;
+	}
+
+
     return 0;
+}
+
+std::string getPath()
+{
+	std::string path;
+	std::getline(std::cin, path);
+	return path;
 }
