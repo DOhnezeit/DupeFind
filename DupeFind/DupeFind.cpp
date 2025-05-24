500f01212b00f84d39f45fbf8bdabd75427660d2
DOhnezeit <dennis.ohnezeit@gmail.com>
2025-05-24 22:28:58 +0200

    Add project files.

---
+++ DupeFind/DupeFind.cpp
@@ -0,0 +1,10 @@
+// Project: DupeFind, Find duplicate files in a directory
+
+#include <iostream>
+#include <filesystem>
+
+int main()
+{
+    std::cout << "DupeFind Duplicate File Finder Starting..." << std::endl;
+    return 0;
+}
